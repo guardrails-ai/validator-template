d5170fc5949c64d2c5cf1c4246a949a9d56107da
Zayd Simjee <zaydsimjee@gmail.com>
2024-01-23 15:50:48 -0800

    add fail scenario to tests

--- test/test-validator.py
+++ test/test-validator.py
@@ -4,17 +4,35 @@ from validator import RegexMatch
 
 
 class ValidatorTestObject(BaseModel):
-    test_val: str = Field(validators=[RegexMatch("a.*")])
+    test_val: str = Field(
+        validators=[
+            RegexMatch(regex="a.*", match_type="fullmatch", on_fail="exception")
+        ]
+    )
 
 
 TEST_OUTPUT = """
 {
-  "test_val": "a string"
+  "test_val": "a test value"
 }
 """
+
 
 guard = Guard.from_pydantic(output_class=ValidatorTestObject)
 
 raw_output, guarded_output, *rest = guard.parse(TEST_OUTPUT)
 
 print("validated output: ", guarded_output)
+
+
+TEST_FAIL_OUTPUT = """
+{
+"test_val": "b test value"
+}
+"""
+
+try:
+  guard.parse(TEST_FAIL_OUTPUT)
+  print ("Failed to fail validation when it was supposed to")
+except (Exception):
+  print ('Successfully failed validation when it was supposed to')
