eb36462a9f5e1fa98758b36c7d6e2b9018c60dcc
zsimjee <zaydsimjee@gmail.com>
2024-02-01 18:38:48 -0800

    Update test-validator.py

--- test/test-validator.py
+++ test/test-validator.py
@@ -7,11 +7,11 @@ from validator import RegexMatch
 guard = Guard.from_string(validators=[RegexMatch(regex="a.*", match_type="fullmatch", on_fail="filter")])
 
 def test_pass():
-  TEST_OUTPUT = "a test value"
+  test_output = "a test value"
   raw_output, guarded_output, *rest = guard.parse(TEST_OUTPUT)
-  assert(guarded_output is TEST_OUTPUT)
+  assert(guarded_output is test_output)
 
 def test_fail():
-  TEST_FAIL_OUTPUT = "b test value"
-  raw_output, guarded_output, *rest = guard.parse(TEST_FAIL_OUTPUT)
+  test_output = "b test value"
+  raw_output, guarded_output, *rest = guard.parse(test_output)
   assert(guarded_output is None)
