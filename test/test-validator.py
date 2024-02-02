7beea197df98edcdec2f68de8b12537ff230cc96
zsimjee <zaydsimjee@gmail.com>
2024-02-01 18:47:10 -0800

    filter -> refrain

--- test/test-validator.py
+++ test/test-validator.py
@@ -4,7 +4,11 @@
 from guardrails import Guard
 from validator import RegexMatch
 
-guard = Guard.from_string(validators=[RegexMatch(regex="a.*", match_type="fullmatch", on_fail="filter")])
+# We use 'refrain' as the validator's fail action,
+#  so we expect failures to always result in a guarded output of None
+# Learn more about corrective actions here:
+#  https://www.guardrailsai.com/docs/concepts/output/#%EF%B8%8F-specifying-corrective-actions
+guard = Guard.from_string(validators=[RegexMatch(regex="a.*", match_type="fullmatch", on_fail="refrain")])
 
 def test_pass():
   test_output = "a test value"
